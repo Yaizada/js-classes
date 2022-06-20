ecfa1ff1600f7d8b2b4360791bf91762104238b4
Aizada Ybykeeva <ybykeeevaaizada@gmail.com>
2022-06-20 12:01:19 +0600

    my commit

---
+++ ageCalculat/main.js
@@ -0,0 +1,8 @@
+let currYear = 2022;
+let birthYear1 = 1983;
+let birthYear2 = 1984;
+let age = currYear-birthYear1;
+let age1 = currYear-birthYear2;
+
+console.log(' They are either '+ age+ ' or '+ age1)
+
